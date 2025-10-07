3cb43aef135165c8184720feea05a25da41a167e
josue <josuejero@hotmail.com>
2025-10-07 06:24:51 -0400

    pwa: register SW only in production

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import App from "./App.tsx";
 import "./styles.css";
 
 createRoot(document.getElementById("root")!).render(
@@ -8,3 +8,9 @@ createRoot(document.getElementById("root")!).render(
     <App />
   </StrictMode>,
 );
+
+if (typeof window !== "undefined" && import.meta.env.PROD) {
+  import("virtual:pwa-register").then(({ registerSW }) => {
+    registerSW({ immediate: false });
+  });
+}
