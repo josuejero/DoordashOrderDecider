1182c66449a3f55c112aafcf737ff078bac3f91f
josue <josuejero@hotmail.com>
2026-01-20 15:33:55 -0500

    update

--- infra/docker/init-db.sql
+++ infra/docker/init-db.sql
@@ -1,2 +1,6 @@
+-- Local + CI databases used across services
 CREATE DATABASE doordash_decider_test;
+CREATE DATABASE doordash_decider_ci_test;
+
 CREATE DATABASE doordash_decision_engine;
+CREATE DATABASE decision_engine_ci;
